4c9bd901540ad36ce4e8b95868c85e08a07c9c45
Ido <games@ido.wtf>
2020-01-11 09:24:21 +0200

    webgl links

---
+++ Assets/Plugins/openPage.jslib
@@ -0,0 +1,7 @@
+mergeInto(LibraryManager.library, {
+  openPage: function (url) {
+    url = Pointer_stringify(url);
+    console.log('Opening link: ' + url);
+    window.open(url,'_blank');
+  }
+});
--- Assets/Scripts/OpenURL.cs
+++ Assets/Scripts/OpenURL.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿#if UNITY_WEBGL
+using System.Runtime.InteropServices;
+#endif    
 using UnityEngine;
 
 public class OpenURL : MonoBehaviour
 {
+#if UNITY_WEBGL
+    [DllImport("__Internal")]
+    private static extern void openPage(string url);
+#endif    
     public string url;
 
     public void Action()
     {
+#if !UNITY_WEBGL
         Application.OpenURL(url);
+#else
+        openPage(url);
+#endif
     }
 }
