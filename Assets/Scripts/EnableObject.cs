ae364b42ea616e40fdc57c0f463147e87523688e
Ido <games@ido.wtf>
2020-01-11 07:58:53 +0200

    Added about

---
+++ Assets/Scripts/EnableObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnableObject : MonoBehaviour {
+
+	public Sprite buttonPressed;
+	private Sprite buttonUp;
+	public GameObject target;
+
+	// Use this for initialization
+	private void Start () {
+		if (GetComponent<SpriteRenderer>() != null)
+			buttonUp = GetComponent<SpriteRenderer> ().sprite;
+	}
+
+	private void OnMouseEnter()
+	{
+		if (buttonPressed != null)
+			GetComponent<SpriteRenderer>().sprite = buttonPressed;
+	}
+
+	private void OnMouseExit()
+	{
+		if (buttonUp != null)
+			GetComponent<SpriteRenderer>().sprite = buttonUp;
+	}
+
+	private void OnMouseDown()
+	{
+		if (buttonPressed != null)
+			GetComponent<SpriteRenderer>().sprite = buttonPressed;
+	}
+
+	private void OnMouseUp()
+	{
+		if (buttonUp != null)
+			GetComponent<SpriteRenderer>().sprite = buttonUp;
+		target.SetActive(true);
+	}
+}
